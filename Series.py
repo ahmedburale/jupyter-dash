c422f5f3bf8a20f32bf8ad9942e709578aafa000
Ahmed Burale <catabilar@gmail.com>
2023-08-29 14:24:43 -0600

    pandas-component-1: updated elements in dataframes

--- Series.py
+++ Series.py
@@ -2,3 +2,22 @@ import pandas as pd
 
 s = pd.Series([42, 21, 7, 3.5])
 print(s)
+
+
+
+# Create a DataFrame
+df = pd.DataFrame({'age':[18,25,39],'name':['Ahmed', 'Aisha','Mary'], 'Occupation':['Student', 'Instructor','Principal']})
+print(df)
+
+#Select by Column
+print(df['age'])
+
+#Select by Index and Slice
+print(df[2:3])
+print(df.iloc[2,1])
+
+#boolean indexing
+print(df[df['age']>30])
+
+#Select by Label
+print(df.loc[:, 'name'])
